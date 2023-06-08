dc0eb76d29fb6cbd32e8e3682b072dc26e60d278
sohelranalive <sohel.cse37@gmail.com>
2023-06-09 01:40:32 +0600

    added JWT

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 //mongodb connection setup
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -28,12 +43,46 @@ async function run() {
         // await client.connect();
         const userCollection = client.db('easyMoves').collection('user');
 
+        // Level wise user (admin, user, instructor) verify
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+            next()
+        }
+
+        const verifyUser = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            if (user?.role !== 'user') {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+            next()
+        }
+
+        const verifyInstructor = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            if (user?.role !== 'instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+            next()
+        }
+
+
+
         //Save all user into DB (including normal user,instructor & admin)
         app.post('/user', async (req, res) => {
             const userInfo = req.body;
             const result = await userCollection.insertOne(userInfo)
             res.send(result)
         })
+
         //return user based on condition
         app.get('/user', async (req, res) => {
             if (req.query?.email) {
@@ -50,6 +99,28 @@ async function run() {
             const allUser = await userCollection.find().toArray()
             res.send(allUser)
         })
+
+        //jwt verify API
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5hr' })
+            res.send({ token })
+        })
+
+        //return the level on authentication
+        app.get('/user/level/:email', verifyJWT, async (req, res) => {
+            const userEmail = req.params.email
+
+            if (req.decoded.email !== userEmail) {
+                return res.status(401).send({ error: true, message: 'unauthorized access' })
+            }
+
+            const query = { email: userEmail }
+            const user = await userCollection.findOne(query)
+            const userRole = user.role;
+            return res.send({ level: userRole })
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
